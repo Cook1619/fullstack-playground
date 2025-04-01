a1fa91c2f94e0924ce2c7373263af04e9eeed526
Mathew Cook <cook6348@gmail.com>
2025-04-01 06:59:20 -0500

    feat: test

--- server/src/controllers/tasks.controller.ts
+++ server/src/controllers/tasks.controller.ts
@@ -4,6 +4,7 @@ import * as tasksService from '../services/tasksService'
 
 export const getTasks = (req: Request, res: Response) => {
     const data = tasksService.getTasks()
+    console.log('we here')
     res.send(data)
 }
 
